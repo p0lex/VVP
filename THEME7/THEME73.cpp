42d2c7a94e6eaf83ac4612af0977491396af1e1b
Alexandra Poluektova <90984216+p0lex@users.noreply.github.com>
2021-12-04 14:52:19 +0300

    Add files via upload

---
+++ THEME7/THEME73.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, a, y;
+	cout <<"Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет." << "\n" << "Вес конфет:";
+	cin >> x;
+	cout << "Стоимость:";
+	cin >> a;
+	cout << "Новый вес конфет:";
+	cin >> y;
+	cout << "Стоимость 1 кг:" << a / x << "\n" << "Стоимость " << y << " кг конфет:"<<a/x*y;
+}
---
+++ THEME7/THEME75.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b;
+	cout <<"Решить линейное уравнение A·x + B = 0, заданное своими коэффициентами A и B (коэффициент A не равен 0)." << "\n" << "Коэффициент А (А<>0):";
+	cin >> a;
+	cout << "Коэффициент В:";
+	cin >> b;
+	cout << "Решение уравнения:" << -b / a;
+}
